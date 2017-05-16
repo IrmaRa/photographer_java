c4eb1bb5a04697b015f7d9308f39f7cb3279f375
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-05-16 19:12:47 +0100

    Add a photographer class with an array of cameras

---
+++ Photographer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Photographer{
+  
+  private String name;
+  private ArrayList<Camera> collection;
+
+  public Photographer(String name){
+    this.name = name;
+    this.camera = new ArrayList<Camera>();
+  }
+
+}
